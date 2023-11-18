c06e67a3a1d20f4f332b7e9f473d957f5f32e219
larasendev <syedrazamehdi1990@gmail.com>
2023-11-18 23:45:56 +0500

    Programming Constructs

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,78 @@ Route::get(
         return "This is the sum of two numbers provided here: ".$number1 + $number2;
     }
 );
+
+
+Route::get(
+    '/programming-constructs',
+    function () {
+        // functions
+
+        // variables (integer, double, string, array, objects)
+        // constants
+        // loops (for, foreach, while, do-while)
+        // return statements (echo, var_dump, dd, print, print_r)
+        // exception handles
+        // conditionals (if, else-if, else)
+
+
+        $name = 'Syed Raza Mehdi Naqvi';                  // string
+        echo $name;
+        $numberOfStepsYouWalkEveryday = 1000;             // integer
+        echo $numberOfStepsYouWalkEveryday;
+        $petrolPrice = 280.84;                            // double/float
+        $peopleToMeet = ['Raza Mehdi', 'Sajjad Askari'];  // array
+        $person = '{
+            "first_name": "Raza",
+            "last_name": "Mehdi"
+        }';   // object
+
+
+
+//        echo $to.'<br>';
+//        $address = '342, Ravi Block AI Town, Lahore';
+//        echo $address.'<br>';
+//        $first_name = 'Sajjad';
+//        $first_prime_number = 2;
+//        $firstPrimeNumber = 2;
+//        const $PI = 3.14;
+
+        // 2 x 1 = 2
+        // 2 x 2 = 4
+        // 2 x 3 = 6
+        // 2 x 4 = 8
+        // 2 x 5 = 10
+        // ...
+        // 2 x 10 = 20
+
+
+
+        // 100 x 1 = 100
+        // 100 x 2 = 200
+        // 100 x 3 = 300
+        // 100 x 4 = 400
+        // 100 x 5 = 500
+        // ...
+        // 100 x 10 = 1000
+
+        // =       assignment operator ()
+        // ==
+        // ===
+
+        // number ki balti <- 1 liter
+        for (
+            $number = 1;      // Your first number will be 1
+            $number <= 10;    // repeat the process until the $number exceeds 10
+            $number++
+            // Whenever the operation inside the body of loop
+            // happens after that this operation should be done
+        ) {
+            echo '<br>'."$number x 100 = ".($number * 100);  // 8   Loop's body
+        }
+
+
+
+
+
+    }
+);
